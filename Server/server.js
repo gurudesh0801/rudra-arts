94901d823858df27dbe1553d9fd8f4a1aa1c61b6
amit <amit@kalpaninfonomics.com>
2025-07-22 09:20:10 +0530

    changes

--- Server/server.js
+++ Server/server.js
@@ -19,29 +19,17 @@ dotenv.config();
 
 const app = express();
 
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://rudra-arts.vercel.app",
-  "https://rudraartsandhandicrafts.in",
-  "https://www.rudraartsandhandicrafts.in",
-];
-
 app.use(
   cors({
-    origin: (origin, callback) => {
-      // allow requests with no origin (like mobile apps, Postman)
-      if (!origin) return callback(null, true);
-
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, origin); // ✅ return the matched origin
-      } else {
-        return callback(new Error("CORS policy violation"));
-      }
-    },
+    origin: [
+      "http://localhost:5173",
+      "https://rudra-arts.vercel.app",
+      "https://rudraartsandhandicrafts.in",
+      "https://www.rudraartsandhandicrafts.in",
+    ],
     credentials: true,
   })
 );
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
