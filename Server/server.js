a596883931276e17bd74b12119021a0d8d30860f
amit <amit@kalpaninfonomics.com>
2025-07-22 09:14:41 +0530

    changes

--- Server/server.js
+++ Server/server.js
@@ -1,61 +1,22 @@
-// server.js
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-import mongoose from "mongoose";
-import cookieParser from "cookie-parser";
-
-// Routes
-import galleryRoutes from "./routes/gallery.js";
-import aboutRoutes from "./routes/about.js";
-import servicesRoutes from "./routes/services.js";
-import contactRoutes from "./routes/contact.js";
-import adminRoutes from "./routes/admin.js";
-import productRoutes from "./routes/products.js";
-import newsRoutes from "./routes/news.js";
-import blogRoutes from "./routes/blog.js";
-
-dotenv.config();
-
-const app = express();
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://rudra-arts.vercel.app",
+  "https://rudraartsandhandicrafts.in",
+  "https://www.rudraartsandhandicrafts.in",
+];
 
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://rudra-arts.vercel.app",
-      "https://rudraartsandhandicrafts.in",
-      "https://www.rudraartsandhandicrafts.in",
-    ],
+    origin: (origin, callback) => {
+      // allow requests with no origin (like mobile apps, Postman)
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, origin); // ✅ return the matched origin
+      } else {
+        return callback(new Error("CORS policy violation"));
+      }
+    },
     credentials: true,
   })
 );
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
-
-// MongoDB connection
-
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.error("MongoDB connection failed:", err));
-
-// Use Routes
-app.use("/api/gallery", galleryRoutes);
-app.use("/api/about", aboutRoutes);
-app.use("/api/services", servicesRoutes);
-app.use("/api/contact", contactRoutes);
-app.use("/api/admin", adminRoutes);
-app.use("/api/products", productRoutes);
-app.use("/api/news", newsRoutes);
-app.use("/api/blogs", blogRoutes);
-
-app.get("/", (req, res) => {
-  res.send("Rudra Arts API running with MongoDB");
-});
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
