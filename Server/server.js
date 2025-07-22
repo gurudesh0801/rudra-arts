05e2a4c961590398a5c198e3ec7d39f3cd798419
amit <amit@kalpaninfonomics.com>
2025-07-22 09:41:47 +0530

    changes

--- Server/server.js
+++ Server/server.js
@@ -33,7 +33,9 @@ app.use(
         callback(new Error("Not allowed by CORS"));
       }
     },
-    credentials: true,
+    methods: "GET,POST,PUT,DELETE,OPTIONS",
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: false,
   })
 );
 app.use(express.json());
